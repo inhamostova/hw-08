84313f1e14d3ea970c29900dd9f023ade5bba7e3
inhamostova <inha.mostova@gmail.com>
2026-06-30 18:43:12 +0300

    add second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const TIME_KEY = 'videoplayer-current-time';
+
+const time = localStorage.getItem(TIME_KEY) ?? 0;
+
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+player
+  .setCurrentTime(time)
+  .then(function (seconds) {
+    console.log(time);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(
+          'the time was less than 0 or greater than the video’s duration'
+        );
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds, duration }) {
+    if (seconds === duration) {
+      localStorage.setItem(TIME_KEY, 0);
+      return;
+    }
+    localStorage.setItem(TIME_KEY, seconds);
+  }, 1000)
+);
